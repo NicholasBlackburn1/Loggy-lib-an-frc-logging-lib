5a84bb69790e0ad73576cbbd47f14265793fabca
nicholas blackburn <nickblackburn02@gmail.com>
2019-11-15 08:54:18 -0500

    added Loggie init

---
+++ src/main/java/Loggie/Example/Exampleloggie.java
@@ -0,0 +1,16 @@
+package Loggie.Example;
+
+import Loggie.lib.Loggie_Logger;
+
+public class Exampleloggie{
+
+    Loggie_Logger logger;
+
+    public void Init(){
+
+        
+       
+    }
+
+    
+}
